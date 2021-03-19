f168c44efc16feafcd0bd41825705d4379528155
Tortique <adrien86190@yahoo.fr>
2021-03-19 12:02:53 +0100

    #1 Project Config

---
+++ src/test/java/com/paymybuddy/p6/PayMyBuddyTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.p6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
