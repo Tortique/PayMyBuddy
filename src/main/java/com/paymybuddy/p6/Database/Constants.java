e6cbfa82ed62d71673097bb0c521bae0bdf7dc5f
Tortique <adrien86190@yahoo.fr>
2021-04-01 09:42:03 +0200

    #6 Sign Up

--- src/main/java/com/paymybuddy/p6/Database/Constants.java
+++ src/main/java/com/paymybuddy/p6/Database/Constants.java
@@ -1,7 +1,6 @@
 package com.paymybuddy.p6.Database;
 
 public class Constants {
-    public static final String SaveUser = "insert into users(userId, email, password, name) values(?,?,?,?)";
-    public static final String CheckLogin = "select * from users where email=? and password=?";
-    public static final String GetUser = "select email from users where email=?";
+    public static final String SaveUser = "insert into users(email, password, name, enabled) values(?,?,?,?)";
+    public static final String GetEmail = "select email from users where email=?";
 }
